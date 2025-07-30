84f83b3d4991b3f3567f4ad8bca75da720ec9787
ldur <lassedur@gmail.com>
2025-07-30 15:14:57 +0200

    Initial Commit

---
+++ Nice PlacesTests/Nice_PlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Nice_PlacesTests.swift
+//  Nice PlacesTests
+//
+//  Created by Lasse Durucz on 30/07/2025.
+//
+
+import Testing
+@testable import Nice_Places
+
+struct Nice_PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
